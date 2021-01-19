fd27cfbbb15346c94de889aa2a8f50c70299bfa1
1098160709 <1098160709@qq.com>
2021-01-19 16:35:56 +0800

    新增时钟主页（demo） 登录后显示的菜单根据权限控制

--- js/lay-module/layuimini/miniAdmin.js
+++ js/lay-module/layuimini/miniAdmin.js
@@ -32,7 +32,7 @@ layui.define(["jquery", "miniMenu", "element","miniTab", "miniTheme"], function 
          * @param options.maxTabNum 最大的tab打开数量
          */
         render: function (options) {
-            options.iniUrl = options.iniUrl || null;
+            var data = options.iniUrl || null;
             options.clearUrl = options.clearUrl || null;
             options.urlHashLocation = options.urlHashLocation || false;
             options.bgColorDefault = options.bgColorDefault || 0;
@@ -41,41 +41,38 @@ layui.define(["jquery", "miniMenu", "element","miniTab", "miniTheme"], function 
             options.loadingTime = options.loadingTime || 1;
             options.pageAnim = options.pageAnim || false;
             options.maxTabNum = options.maxTabNum || 20;
-            $.getJSON(options.iniUrl, function (data) {
-                if (data == null) {
-                    miniAdmin.error('暂无菜单信息')
-                } else {
-                    miniAdmin.renderLogo(data.logoInfo);
-                    miniAdmin.renderClear(options.clearUrl);
-                    miniAdmin.renderHome(data.homeInfo);
-                    miniAdmin.renderAnim(options.pageAnim);
-                    miniAdmin.listen();
-                    miniMenu.render({
-                        menuList: data.menuInfo,
-                        multiModule: options.multiModule,
-                        menuChildOpen: options.menuChildOpen
-                    });
-                    miniTab.render({
-                        filter: 'layuiminiTab',
-                        urlHashLocation: options.urlHashLocation,
-                        multiModule: options.multiModule,
-                        menuChildOpen: options.menuChildOpen,
-                        maxTabNum: options.maxTabNum,
-                        menuList: data.menuInfo,
-                        homeInfo: data.homeInfo,
-                        listenSwichCallback: function () {
-                            miniAdmin.renderDevice();
-                        }
-                    });
-                    miniTheme.render({
-                        bgColorDefault: options.bgColorDefault,
-                        listen: true,
-                    });
-                    miniAdmin.deleteLoader(options.loadingTime);
-                }
-            }).fail(function () {
-                miniAdmin.error('菜单接口有误');
-            });
+
+            if (data == null) {
+                miniAdmin.error('暂无菜单信息')
+            } else {
+                miniAdmin.renderLogo(data.logoInfo);
+                miniAdmin.renderClear(options.clearUrl);
+                miniAdmin.renderHome(data.homeInfo);
+                miniAdmin.renderAnim(options.pageAnim);
+                miniAdmin.listen();
+                miniMenu.render({
+                    menuList: data.menuInfo,
+                    multiModule: options.multiModule,
+                    menuChildOpen: options.menuChildOpen
+                });
+                miniTab.render({
+                    filter: 'layuiminiTab',
+                    urlHashLocation: options.urlHashLocation,
+                    multiModule: options.multiModule,
+                    menuChildOpen: options.menuChildOpen,
+                    maxTabNum: options.maxTabNum,
+                    menuList: data.menuInfo,
+                    homeInfo: data.homeInfo,
+                    listenSwichCallback: function () {
+                        miniAdmin.renderDevice();
+                    }
+                });
+                miniTheme.render({
+                    bgColorDefault: options.bgColorDefault,
+                    listen: true,
+                });
+                miniAdmin.deleteLoader(options.loadingTime);
+            }
         },
 
         /**
