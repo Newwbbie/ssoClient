9156a9b8052fa9e0120bb384661b691371ad18a3
1098160709 <1098160709@qq.com>
2021-03-04 17:32:47 +0800

    加入5G前传，施工中 进度：show_sys.html

--- js/public.js
+++ js/public.js
@@ -17,7 +17,23 @@ function mAjax(data, url, type, success) {
             window.parent.layui.layer.msg('登录过期，即将返回登录页面', {
                 time: 1000  // 1秒关闭
             }, function () {
-                window.location = 'page/login-1.html';
+                // window.location = 'page/login-1.html';
+            });
+        }
+    });
+}
+
+function mGET(url, success) {
+    $.ajax({
+        url: url,
+        type: 'GET',
+        headers: { token: token },
+        success: success,
+        error: function() {
+            window.parent.layui.layer.msg('登录过期，即将返回登录页面', {
+                time: 1000  // 1秒关闭
+            }, function () {
+                // window.location = 'page/login-1.html';
             });
         }
     });
@@ -73,4 +89,40 @@ function getOneRow() {
     } else {
         return checkStatus.data[0];
     }
+}
+
+// datagrid封装
+function mDatagrid(dom, url, data, cols,
+                   pageSize = 20,
+                   pageList = [10,20,30,40,50],
+                   sync = false) {
+    $(dom).datagrid({
+        url: url,
+        headers: {token: token},
+        contentType: "application/json;charset=utf-8;",
+        striped: true, 		// 奇偶行使用不同背景色
+        pagination: true,	// 分页工具栏
+        rownumbers: true,	// 初始化页码
+        pageSize: pageSize,
+        pageList: pageList,
+        singleSelect: true,	// 单选
+        fitColumns: false,	// 自适应表格宽度
+        emptyMsg: "列表为空",
+        remoteSort: false,
+        multiSort: true,
+        sync: sync,
+        queryParams: data,
+        method: "GET",
+        // 实现选择单行，取消单行
+        onBeforeSelect: function(index, row){
+            var row = $(dom).datagrid('getSelected');
+            var curRowindex = $('#dg').datagrid('getRowIndex',row);
+            if(curRowindex != index) {return true;}
+            else{ $('#dg').datagrid('unselectRow',index); return false;}
+        },
+        columns: cols,
+        onloadSuccsee: function (data) {
+            $("#dg").datagrid('resize');
+        }
+    });
 }
